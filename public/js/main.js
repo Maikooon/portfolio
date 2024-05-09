88b20d9361a8344d0f9a1704b982691d36fc48a7
kate0626 <158990209+kate0626@users.noreply.github.com>
2024-05-09 12:43:10 +0900

    change image size

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,50 @@
 // 関数を定義
-function fadeAnime(){    //関数の宣言  
-  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラスそれぞれに足して処理を行う
-    let elemPos = $(this).offset().top-50;//要素より、50px上の
+function fadeAnime() {    //関数の宣言  
+  $('.fadeUpTrigger').each(function () { //fadeUpTriggerというクラスそれぞれに足して処理を行う
+    let elemPos = $(this).offset().top - 50;//要素より、50px上の
     let scroll = $(window).scrollTop();
     let windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
+    if (scroll >= elemPos - windowHeight) {
       $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-    }else{
+    } else {
       $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
     }
-    });
+  });
 }
 
 
 // 関数を実行
 // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function(){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+// modalの操作を管理
+$(function () {
+  $('#openModal').click(function () {
+    $('#modalArea').fadeIn();
+  });
+  $('#closeModal , #modalBg').click(function () {
+    $('#modalArea').fadeOut();
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const personImg = document.querySelector('.person-img img'); // 画像要素を取得
+
+  // 画像をクリックしたときのイベントリスナーを追加
+  personImg.addEventListener('click', function () {
+    const modal = document.querySelector('.js-modal'); // モーダル要素を取得
+    modal.classList.add('is-open'); // モーダルを表示するためのクラスを追加
+  });
+});
+
 
 
 
